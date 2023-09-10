c5a9b61f5aee468afcb20b646811afd38a121ea4
enansari <rahmat2022a@gmail.com>
2023-09-10 19:29:34 +0330

    fix: archive 1401 samples

--- src/001/main.cpp
+++ src/001/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
+int main() {
+    cout << "Hello World" << endl;
 
-    int num;
-    cout << "\nEnter number: ";
-    cin >> num;
-
-    int rev_num = 0;
-    while (num > 0) {
-        rev_num = rev_num * 10 + num % 10;
-        num = num / 10;
-    }
-
-    cout << "\nReverse of no. is " << rev_num;
-
-	return 0;
+    return 0;
 }
