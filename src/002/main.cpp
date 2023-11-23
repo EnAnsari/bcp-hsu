722589fc8a016af7e1a58f2c77fb03ed2f457388
enansari <rahmat2022a@gmail.com>
2023-11-23 12:49:25 +0330

    adding answer of question 2

---
+++ src/002/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Area: " << a * b << endl;
+    cout << "perimeter: " << 2 * (a + b) << endl;
+
+    return 0;
+}
