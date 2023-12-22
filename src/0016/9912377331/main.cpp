d825cb78963fe44fb31e0b30dafee9bc3e9e6cce
enansari <rahmat2022a@gmail.com>
2023-12-22 16:53:56 +0330

    adding answer of binary search

--- src/0016/9912377331/main.cpp
+++ src/0016/9912377331/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int binarySearch(int arr[], int l, int r, int x) {
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == x)
+            return m;
+        if (arr[m] < x)
+            l = m + 1;
+        else
+            r = m - 1;
+    }
+    return -1;
+}
+
 int main() {
-    cout << "Hello World" << endl;
+    int n, x;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    cin >> x;
+    cout << binarySearch(arr, 0, n - 1, x);
 
     return 0;
 }
