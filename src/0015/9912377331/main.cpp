3a5ad486f6eeb502b0ed8bafa8a40603e3e7b94e
enansari <rahmat2022a@gmail.com>
2023-12-22 16:47:23 +0330

    adding answer of bubble sort

--- src/0015/9912377331/main.cpp
+++ src/0015/9912377331/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
 int main() {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    bubbleSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
