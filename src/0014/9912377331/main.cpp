0fcadbe93a6a85d07e036ee97a24c7f8f2c66436
enansari <rahmat2022a@gmail.com>
2023-12-22 16:44:50 +0330

    adding answer of selection sort

--- src/0014/9912377331/main.cpp
+++ src/0014/9912377331/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
+}
+
 int main() {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    selectionSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
