4d5b82ce1617e6210613a0b1139369372e46ce3f
AliakbarSoli <aliakbarsoltaniiiii@gmail.com>
2023-12-28 21:22:21 +0330

    Answer 2023 Que

---
+++ src/2023/4021277130/main.py
@@ -0,0 +1,29 @@
+n=int(input())
+x=0
+y=0
+z=0
+w=n
+i=0
+while i<w :
+    j=0
+    while j<w :
+        w=n-(i+j)
+        if pow(w,2)==pow(i,2)+pow(j,2) :
+            x=i
+            y=j
+            z=w
+        j+=1
+    i+=1
+
+if x!=0  :
+    if x>=y :
+        print(y," ")
+        print(x," ")
+        print(z)
+    else :
+        print(y," ")
+        print(x," ")
+        print(z)
+else :
+    print("Impossible")
+   
