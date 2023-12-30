e4db7ccf37eacccb5a77fe6899b1c0e1529cdc8c
Sajjad Esmaily <149365963+Sajjad-esmaily@users.noreply.github.com>
2023-12-30 17:49:01 +0330

    Create main.cpp

---
+++ src/3002/4021277298/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+int main(){
+int Ia,Ja;
+cin>>Ia>>Ja;
+int A[Ia][Ja];
+for (int i = 0; i < Ia; i++)
+{
+    for (int j = 0; j < Ja; j++)
+    {
+        cin>>A[i][j];
+    }
+}
+// A
+int max = A[0][0];
+for (int i = 0; i < Ia; i++)
+{
+    for (int j = 0; j < Ja; j++)
+    {
+        if (A[i][j] > max)
+        {
+            max = A[i][j];
+        }
+    }
+}
+cout<<max<<endl;
+// A
+// B
+int max = A[0][0];
+for (int i = 0; i < Ia; i++)
+{
+    for (int j = 0; j < Ja; j++)
+    {
+        if (i == j)
+        {
+            if (A[i][j] > max)
+            {
+                max = A[i][j];
+            }   
+        }
+    }
+}
+cout<<max<<endl;
+// B
+// C
+int max = A[0][1],vector = -1;
+for (int i = 0; i < Ia; i++)
+{
+    for (int j = 0; j < Ja; j++)
+    {
+        if (i == j)
+        {
+            vector++;
+        }
+        if (i > vector && j > vector)
+        {
+            if (A[i][j] > max)
+            {
+                max = A[i][j];
+            }
+        }   
+    }
+}
+cout<<max<<endl;
+// C
+// D
+int max = A[1][0], vector = -1;
+for (int i = 0; i < Ia; i++)
+{
+    for (int j = 0; j < Ja; j++)
+    {
+        if (i == j)
+        {
+            vector++;
+        }
+        if (i < vector && j < vector)
+        {
+            if (A[i][j] > max)
+            {
+                max = A[i][j];
+            }
+        }   
+    }
+}
+cout<<max<<endl;
+// D
+    return 0;
+}
