35ca0e5818decb9ced0e2588b5386a3a2f179900
abolfazl <154689428+abolfazl-ghoo@users.noreply.github.com>
2023-12-30 11:42:16 +0330

    Create main.cpp

---
+++ src/1003/4021277038/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the Number: ";
+    cin>>a;
+    if (a%2==0)
+    {
+        cout<<"The number is even";
+    }
+    if (a%2!=0)
+    {
+        cout<<"is an odd number";
+    }
+
+return 0;
+}
