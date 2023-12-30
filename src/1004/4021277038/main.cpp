fa2d26d589799dfba349b5ca748aedb28673a6f8
abolfazl <154689428+abolfazl-ghoo@users.noreply.github.com>
2023-12-30 11:52:17 +0330

    Update main.cpp

--- src/1004/4021277038/main.cpp
+++ src/1004/4021277038/main.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,s;
+    cout<<"the watch: ";
+    cin>>a;
+    cout<<"minutes: ";
+    cin>>b;
+    cout<<"second: ";
+    cin>>c;
+    s=(a*3600)+(b*60)+c;
+    cout<<s<<" seconds of the day pass";
 
+return 0;
+}
